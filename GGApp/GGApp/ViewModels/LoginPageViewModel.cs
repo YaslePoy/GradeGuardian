550f129c9bfb456f7f83bdf72350382284f05a84
Mihail Mitrovanov <mmm606@inbox.ru>
2025-09-18 15:54:27 +0300

    Add tt page

--- GGApp/GGApp/ViewModels/LoginPageViewModel.cs
+++ GGApp/GGApp/ViewModels/LoginPageViewModel.cs
@@ -21,28 +21,26 @@ public class LoginPageViewModel : ReactiveObject, IRoutableViewModel
 
     public string Login { get; set; }
     public string Password { get; set; }
-
-    public ICommand GoRegister => ReactiveCommand.CreateFromObservable(() =>
-        HostScreen.Router.Navigate.Execute(new RegisterPageViewModel(HostScreen)));
+    
 
     public ICommand TryAuth => ReactiveCommand.Create(async () =>
     {
-        Debug.WriteLine($"Login: {Login}, password: {Password}");
-        if (await Api.Auth(Login, Password) is { } loginResponse)
-        {
-            if (!Directory.Exists("./UserData")) Directory.CreateDirectory("./UserData");
-            AppContext.CurrentUser = loginResponse;
-
-            var saving = JsonSerializer.Serialize(loginResponse, Api.JsonOptions);
-            Api.Http.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse("Bearer " + AppContext.CurrentUser!.Token);
-            File.WriteAllText("./UserData/user.json", saving);
-            HostScreen.Router.Navigate.Execute(new BasePageViewModel(HostScreen));
-        }
-        else
-        {
-            _incorrectAuth.Add(Login + "+" + Password);
-            this.RaiseAndSetIfChanged(ref _invalidData, true, nameof(InvalidData));
-        }
+        // Debug.WriteLine($"Login: {Login}, password: {Password}");
+        // if (await Api.Auth(Login, Password) is { } loginResponse)
+        // {
+        //     if (!Directory.Exists("./UserData")) Directory.CreateDirectory("./UserData");
+        //     AppContext.CurrentUser = loginResponse;
+        //
+        //     var saving = JsonSerializer.Serialize(loginResponse, Api.JsonOptions);
+        //     Api.Http.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse("Bearer " + AppContext.CurrentUser!.Token);
+        //     File.WriteAllText("./UserData/user.json", saving);
+        //     HostScreen.Router.Navigate.Execute(new BasePageViewModel(HostScreen));
+        // }
+        // else
+        // {
+        //     _incorrectAuth.Add(Login + "+" + Password);
+        //     this.RaiseAndSetIfChanged(ref _invalidData, true, nameof(InvalidData));
+        // }
     });
 
     public bool InvalidData
---
+++ GGApp/GGApp/Views/TimetablePage.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GGApp.Views;
+
+public partial class TimetablePage : UserControl
+{
+    public TimetablePage()
+    {
+        InitializeComponent();
+    }
+}
